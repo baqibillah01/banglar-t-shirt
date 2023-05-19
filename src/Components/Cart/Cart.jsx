91a2efe76cc0fdcf34cf3d043cc72e674d8bce2b
baqibillah01 <baqibilla00@gmail.com>
2023-05-19 21:47:14 +0600

    added toast and message

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -1,11 +1,35 @@
 import React from 'react';
 
-const Cart = () => {
+const Cart = ({cart, handleRemoveFromCart}) => {
+    let message;
+    if(cart.length === 0){
+        message = <p>Please add some products</p>
+    }
+    else{
+        message = <div>
+            <h3>Boroloxx</h3>
+            <p><small>Thanks for giving your money</small></p>
+        </div>
+    }
     return (
         <div>
-            <h3>order summary</h3>
+            <h3>order summary: {cart.length}</h3>
+            {cart.length > 2 ? <span>Aro kino</span> : <span>fakir</span>}
+            {message}
+            {
+                cart.map(tshirt => <p 
+                key={tshirt._id}
+                >{tshirt.name} <button onClick={() => handleRemoveFromCart(tshirt._id)}>X</button>
+                </p>)
+            }
         </div>
     );
 };
 
 export default Cart;
+
+/**
+ * CONDITIONAL RENDERING
+ * 1. Use if else to set a variable that will contain an element, components
+ * 2. ternary operator: condition ? 'for true' : 'false'
+*/
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,11 +1,31 @@
 import React from 'react';
+import { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
 import Cart from '../Cart/Cart';
 import TShirt from '../TShirt/TShirt';
 import './Home.css'
+import Toast, { toast } from 'react-hot-toast';
 
 const Home = () => {
     const tshirts = useLoaderData();
+    const [cart, setCart] = useState([]);
+
+    const handleAddToCart = tshirt =>{
+        const exists = cart.find(ts => ts._id === tshirt._id);
+        if(exists){
+            toast('You have already added this t-shirt');
+        }
+        else{
+            const newCart = [...cart, tshirt];
+            setCart(newCart);
+        } 
+    }
+
+    const handleRemoveFromCart = _id =>{
+        const remaining = cart.filter(ts => ts._id !== _id);
+        setCart(remaining);
+    }
+
     return (
         <div className='home-container'>
             <div className='tshirts-container'>
@@ -13,11 +33,15 @@ const Home = () => {
                 tshirts.map(tshirt => <TShirt
                 key={tshirt._id}
                 tshirt = {tshirt}
+                handleAddToCart = {handleAddToCart}
                 ></TShirt>)
             }
             </div>
             <div className="cart-container">
-                <Cart></Cart>
+                <Cart 
+                cart={cart}
+                handleRemoveFromCart = {handleRemoveFromCart}
+                ></Cart>
             </div>
         </div>
     );
