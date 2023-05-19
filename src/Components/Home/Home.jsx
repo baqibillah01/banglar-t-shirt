943016dc7080d06dcd57ba339278e55f7510507f
baqibillah01 <baqibilla00@gmail.com>
2023-05-19 20:05:53 +0600

    added cart and display

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+import Cart from '../Cart/Cart';
+import TShirt from '../TShirt/TShirt';
+import './Home.css'
 
 const Home = () => {
     const tshirts = useLoaderData();
     return (
-        <div>
-            <h2>This is home: {tshirts.length}</h2>
+        <div className='home-container'>
+            <div className='tshirts-container'>
+            {
+                tshirts.map(tshirt => <TShirt
+                key={tshirt._id}
+                tshirt = {tshirt}
+                ></TShirt>)
+            }
+            </div>
+            <div className="cart-container">
+                <Cart></Cart>
+            </div>
         </div>
     );
 };
